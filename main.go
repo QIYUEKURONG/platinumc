ee028706a2f0ac726064155df4221a89d16f157c
juwenjie <juwenjie@onething.net>
2019-05-07 19:32:00 +0800

    second modify

--- main.go
+++ main.go
@@ -1,13 +1,53 @@
 package main
 
-import "github.com/QIYUEKURONG/platinumc/platinumc"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/QIYUEKURONG/platinumc/platinumc"
+)
+
+func parseCommandLineArguemtns(t *platinumc.Task) {
+	flag.StringVar(&t.Identifier, "x", "", "Client Identifier")
+	flag.StringVar(&t.TransportProtocol, "t", "", "Transport Protocol (tcp, wss)")
+	flag.StringVar(&t.ServerAddress, "a", "", "ServerAddress(tcp, wss, udp or mona address)")
+	flag.StringVar(&t.FileIndex, "f", "", "FileIndex(must not be empty)")
+	flag.UintVar(&t.BlockIndex, "b", 0, "BlockIndex,(default = 0)")
+	flag.StringVar(&t.SavePath, "o", "", "SavePath")
+	flag.UintVar(&t.StartPieceIndex, "s", 0, "StartPieceIndex")
+	flag.UintVar(&t.FetchPieceCount, "n", 0, "FetchPieceCount")
+	flag.BoolVar(&t.VerboseLog, "v", false, "(VerboseLog)")
+	flag.BoolVar(&t.CheckPiece, "c", false, "(CheckPiece)")
+
+	flag.Parse()
+}
+
+func checkCommandLineArguments(t *platinumc.Task) bool {
+	if t.TransportProtocol == "" {
+		fmt.Println("failed! TransportProtocol is empty!")
+		//flag.CommandLine()build
+		return false
+	}
+	if t.ServerAddress == "" {
+		fmt.Println("failed!ServerAddress is empty!")
+		return false
+	}
+	if t.FileIndex == "" {
+		fmt.Println("failed! FileIndex is empty!")
+		return false
+	}
+	return true
+}
 
 func main() {
 	task := platinumc.Task{}
+	parseCommandLineArguemtns(&task)
+	if !checkCommandLineArguments(&task) {
+		flag.PrintDefaults()
+	}
 
 	// Parse command line arguemnts
-
 	// Check task
+	//	platinumc.Run(&task)
 
-	platinumc.Run(&task)
 }
--- platinumc/task.go
+++ platinumc/task.go
@@ -1,5 +1,15 @@
 package platinumc
 
+// Task can record Client task type
 type Task struct {
-	identifier string
+	TransportProtocol string
+	ServerAddress     string
+	FileIndex         string
+	BlockIndex        uint
+	SavePath          string
+	StartPieceIndex   uint
+	FetchPieceCount   uint
+	CheckPiece        bool
+	VerboseLog        bool
+	Identifier        string
 }
